916cf5f8e602ac523d769641db01925a54235971
Xavier Nopre <xnopre@gmail.com>
2025-11-29 16:12:02 +0100

    FIX erreur de compilation dans scripts/seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -10,7 +10,7 @@ if (!MONGODB_URI) {
 
 async function seed() {
   try {
-    await mongoose.connect(MONGODB_URI)
+    await mongoose.connect(MONGODB_URI!)
     console.log('Connected to MongoDB')
 
     await TicketModel.deleteMany({})
