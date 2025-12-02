515a8533225645e772872b056eddf394860151e7
Xavier Nopre <xnopre@gmail.com>
2025-12-02 15:38:03 +0100

    [CI] FIX typages

--- src/application/services/ServiceFactory.ts
+++ src/application/services/ServiceFactory.ts
@@ -2,9 +2,12 @@ import { MongoTicketRepository } from '@/infrastructure/repositories/MongoTicket
 import { TicketService } from './TicketService';
 
 export class ServiceFactory {
-  private static ticketService = new TicketService(new MongoTicketRepository());
+  private static ticketService: TicketService | null = null;
 
   static getTicketService(): TicketService {
+    if (!this.ticketService) {
+      this.ticketService = new TicketService(new MongoTicketRepository());
+    }
     return this.ticketService;
   }
 }
